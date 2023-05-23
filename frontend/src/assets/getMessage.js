96ff343b45a5c1a87f49b99e339a7f91aafc44ba
vladdd0 <vlad123123v@gmail.com>
2023-05-24 01:51:31 +0300

    Changed url

--- frontend/src/assets/getMessage.js
+++ frontend/src/assets/getMessage.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getMessage = async (messageId) => {
     try {
-        const response = await axios.get(`http://localhost:8000/messages/${messageId}`);
+        const response = await axios.get(`http://backend:8000/messages/${messageId}`);
         return response.data;
     } catch (error) {
         console.error(error);
--- frontend/src/assets/getMessages.js
+++ frontend/src/assets/getMessages.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const locale = document.documentElement.lang;
 const getMessages = async(limit, skip) => {
     try {
-        const response = await axios.get(`http://localhost:8000/${locale}/lobby/get?limit=${limit}&skip=${skip}`);
+        const response = await axios.get(`http://backend:8000/${locale}/lobby/get?limit=${limit}&skip=${skip}`);
         return response.data;
     } catch(error) {
         console.log(error)
