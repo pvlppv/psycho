62dccc4714ae780ad2a599d3313f8bc263066c75
vladdd0 <vlad123123v@gmail.com>
2023-05-24 01:48:51 +0300

    Moved to app dir

--- backend/docker/app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-alembic upgrade head
-cd app
-cat .env-non-dev | gunicorn app:app --workers ${BACKEND_WORKERS} --worker-class uvicorn.workers.UvicornWorker --bind=${BACKEND_HOST}:${BACKEND_PORT} --log-level ${BACKEND_LOG_LEVEL}
