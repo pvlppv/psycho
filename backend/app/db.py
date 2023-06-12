5486a05507193632b3d0888577a902f37d2fb5b9
vladdd0 <vlad123123v@gmail.com>
2023-06-12 04:14:11 +0300

    Update database

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,24 +1,22 @@
 from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
+                                    async_sessionmaker, create_async_engine)
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.pool import NullPool
+
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-# database connection
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
-# database accumulation 
 Base = declarative_base()
+metadata = MetaData()
+engine = create_async_engine(url=DATABASE_URL, echo=False)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-# engine
-engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
+def create_all(engine: AsyncEngine) -> None:
+    return metadata.create_all(engine)
 
-# session
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
-
-# управлять жизненным циклом асинхронной сессии базы данных
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
+async def get_async_session() -> AsyncSession:
+    async with async_session() as session:
         yield session
-
