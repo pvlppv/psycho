d99b8e746a3faa74564c8a9f7eda5d060ad4b892
vladdd0 <vlad123123v@gmail.com>
2023-06-12 04:14:44 +0300

    Update app

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,30 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_limiter import FastAPILimiter
+from redis import asyncio as aioredis
 
 from config import REDIS_HOST, REDIS_PORT
-from lobby.routers import router as lobby_router
+from db import Base, engine
+from early_access.routers import router as early_access_router
+
+# from lobby.routers import router as lobby_router
 # from reply.routers import router as reply_router
 # from report.routers import router as report_router
 # from message.routers import router as message_router
-
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-
-from redis import asyncio as aioredis
-
-from fastapi_limiter import FastAPILimiter
-
 
 app = FastAPI(
     title='Thoughty',
     version='1.0',
 )
 
-
 app.include_router(
-    lobby_router,
+    router=early_access_router,
 )
+# app.include_router(
+#     lobby_router,
+# )
 # app.include_router(
 #     reply_router,
 # )
@@ -51,12 +52,8 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=[
-        'GET',
-        'POST',
-        'OPTIONS',
-        'DELETE',
-        'PATCH',
-        'PUT',
+        "POST",
+        "PATCH",
     ],
     allow_headers=[
         'Content-Type',
@@ -67,11 +64,13 @@ app.add_middleware(
         'Accept-Language',
     ],
 )
-# app.add_middleware(HTTPSRedirectMiddleware)
+#app.add_middleware(HTTPSRedirectMiddleware)
 
 
 @app.on_event('startup')
 async def startup_event():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     redis = aioredis.from_url(f'redis://{REDIS_HOST}:{REDIS_PORT}', encoding='utf8', decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix='fastapi-cache')
     await FastAPILimiter.init(redis)
