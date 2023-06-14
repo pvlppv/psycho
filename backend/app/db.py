735a9ba064279e358f80501a6cef6b6a4c05635c
vladdd0 <vlad123123v@gmail.com>
2023-06-14 14:24:45 +0300

    Linted db

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,14 +1,14 @@
-from typing import AsyncGenerator
 
 from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
-                                    async_sessionmaker, create_async_engine)
+                                    create_async_engine)
 from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.pool import NullPool
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
-DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DATABASE_URL = (f"postgresql+asyncpg://"
+                f"{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
 Base = declarative_base()
 metadata = MetaData()
 engine = create_async_engine(url=DATABASE_URL, echo=False)
